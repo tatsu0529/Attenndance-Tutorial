44175b75f8975fa76350ef363cba34190a1da727
tatsu <tatsutatsuyan0529@gmail.com>
2019-11-30 06:31:00 +0000

    finished edit-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,25 +67,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :department, :password, :password_confirmation)
     end
     
-    def set_user
-      @user = User.find(params[:id])
-    end
-    
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-    end
-  end
-  
-    def correct_user
-      redirect_to(root_url) unless current_user?(@user)
-    end
-    
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
     
     def basic_info_params
       params.require(:user) .permit(:department, :basic_time, :work_time)
